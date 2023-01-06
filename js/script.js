d0cf29f1032cc76410fcc58bc23ac1a8a6554c79
kristof1345 <kristofnemeth1345@gmail.com>
2023-01-06 07:34:35 +0100

    completed

--- js/script.js
+++ js/script.js
@@ -1,37 +1,56 @@
+const hamburger = document.getElementById("hamburger");
+const mobileNav = document.querySelector(".flex");
 const shortenedLinks = document.getElementById("shortened-links");
 const link = document.getElementById("link");
 const form = document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
-  shortenLinks();
-  render();
-  clearForm();
+  const url = link.value;
+  shortenLinks(url);
 });
 
-function shortenLinks() {}
+async function shortenLinks(url) {
+  try {
+    const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
+    const data = await res.json();
 
-function render() {
-  let linkBlock = document.createElement("div");
-  linkBlock.classList.add("link-block");
+    let linkBlock = document.createElement("div");
+    linkBlock.classList.add("link-block");
 
-  let originalLink = document.createElement("p");
-  originalLink.classList.add("original-link");
-  originalLink.textContent = link.value;
-  linkBlock.appendChild(originalLink);
+    let originalLink = document.createElement("p");
+    originalLink.classList.add("original-link");
+    originalLink.textContent = link.value;
+    linkBlock.appendChild(originalLink);
 
-  let holderDiv = document.createElement("div");
-  linkBlock.appendChild(holderDiv);
+    let holderDiv = document.createElement("div");
+    holderDiv.innerHTML = `<p class="shortened-link"> ${data.result.short_link}</p>
+                          <button class="copy">Copy!</button>`;
+    linkBlock.appendChild(holderDiv);
 
-  let shortenedLink = document.createElement("p");
-  shortenedLink.classList.add("shortened-link");
-  shortenedLink.textContent = link.value;
-  holderDiv.appendChild(shortenedLink);
+    shortenedLinks.prepend(linkBlock); //using prepend so its adds the whole block before old ones
 
-  let copyButton = document.createElement("button");
-  copyButton.classList.add("copy");
-  copyButton.textContent = "Copy!";
-  holderDiv.appendChild(copyButton);
+    const shortL = shortenedLinks.querySelector(".shortened-link");
+    const copyBtn = shortenedLinks.querySelector(".copy"); // this way we can use a simple query selector on a class so we only select the latest link
+    copyBtn.addEventListener("click", () => {
+      navigator.clipboard.writeText(shortL.textContent);
+      copyBtn.textContent = "Copied!";
+      copyBtn.style.backgroundColor = "hsl(257, 27%, 26%)";
+    });
 
-  shortenedLinks.appendChild(linkBlock);
+    clearForm();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function clearForm() {}
+function clearForm() {
+  let formR = document.querySelector("form");
+  formR.reset();
+}
+
+function toggleNav() {
+  if (mobileNav.classList.contains("none")) {
+    mobileNav.classList.remove("none");
+  } else {
+    mobileNav.classList.add("none");
+  }
+}
